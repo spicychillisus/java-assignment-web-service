4f79fb5ac23ee3f27d1034e1bb5bca368796e4e9
spicychillisus <asherben90@gmail.com>
2025-02-04 14:41:41 +0800

    init

---
+++ src/test/java/com/example/java_assignment_web_service/JavaAssignmentWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_assignment_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssignmentWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
